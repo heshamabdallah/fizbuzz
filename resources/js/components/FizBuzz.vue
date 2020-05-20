eb9aee35892d1a56a822fdb853f8f694e6a3512b
Hesham AbdAllah <hesham.abdallah.ha@gmail.com>
2020-05-21 00:07:31 +0200

    update fizbuzz UI

--- resources/js/components/FizBuzz.vue
+++ resources/js/components/FizBuzz.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        {{ computedFizBuzz }}
+    <div class="pt-3 fizbuzz-container">
+        <div class="container shadow p-5">
+            <div class="main-title text-center mb-3">
+                Fiz-Buzz Test
+            </div>
+            <div class="items-container">
+                <span v-for="(item, itemIndex) in fizBuzz" :key="itemIndex">
+                    <span :class="getItemClass(itemIndex)">
+                        {{ item }}
+                    </span>
+                </span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,16 +21,23 @@ export default {
     data: () => ({
         fizBuzz: []
     }),
-    computed: {
-        computedFizBuzz() {
-            return this.fizBuzz.join(', ')
-        }
-    },
     methods: {
         fetchFizBuzzData() {
             axios.get('/fizbuzz').then(({ data }) => {
                 this.fizBuzz = data.data
             })
+        },
+        getItemClass(itemIndex) {
+            let value = itemIndex + 1
+            if (value % 15 === 0) {
+                return 'item-fizbuzz'
+            } else if (value % 3 === 0) {
+                return 'item-fizz'
+            } else if (value % 5 === 0) {
+                return 'item-buzz'
+            }
+
+            return null
         }
     },
     created() {
@@ -28,6 +46,22 @@ export default {
 }
 </script>
 
-<style type="scss" scoped>
-
+<style lang="scss" scoped>
+.fizbuzz-container {
+    .main-title {
+        font-size: 30px;
+    }
+    .items-container {
+        .item-fizbuzz {
+            color: #43aa8b;
+            font-weight: bold;
+        }
+        .item-fizz {
+            color: #0077b6;
+        }
+        .item-buzz {
+            color: #ee6c4d;
+        }
+    }
+}
 </style>
