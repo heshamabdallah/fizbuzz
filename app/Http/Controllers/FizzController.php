1a4d01b4013580a22bd2e32616731f048eec1819
Hesham AbdAllah <hesham.abdallah.ha@gmail.com>
2020-05-20 23:11:43 +0200

    create fizbuzz endpoint

---
+++ app/Http/Controllers/FizzController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FizzController extends Controller {
+
+    /**
+     * Get FizzBuzz data.
+     *
+     * @var array
+     */
+    public function index() {
+        $data = [];
+
+        for ($i=1; $i <= 100; $i++) { 
+            $value = $i;
+            if ($i % 15 === 0) {
+                $value = 'FizzBuzz';
+            } elseif ($i % 3 === 0) {
+                $value = 'Fizz';
+            } elseif ($i % 5 === 0) {
+                $value = 'Buzz';
+            }
+
+            $data[] = $value;
+        }
+
+        return compact('data');
+    }
+}
